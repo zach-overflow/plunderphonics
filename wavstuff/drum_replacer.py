184c799f6f25b58c01cf37bfddea50ec5e6d9ff9
Zach Gottesman <zigottesman@gmail.com>
2014-04-16 02:39:46 -0700

    drum extraction now works.

--- wavstuff/drum_replacer.py
+++ wavstuff/drum_replacer.py
@@ -31,7 +31,8 @@ while True:
 # 	print onsets[i]
 # 	i += 1
 
-def extract_drums(attack = 200, decay = 4000, onset_array = onsets, wavefile = sys.argv[1]):
+#def extract_drums(attack = 200, decay = 4000, onset_array = onsets, wavefile = sys.argv[1]):
+def extract_drums(onset_array = onsets, wavefile = sys.argv[1]):
 	"""extracts the individual drum hits from a wav file and writes them as separate wav files.
 	   attack and decay are in samples instead of milliseconds. So 44 samples is about 1 millisecond 
 	   for a second recorded at 44.1Khz samplerate.
@@ -40,28 +41,35 @@ def extract_drums(attack = 200, decay = 4000, onset_array = onsets, wavefile = s
 	   slightly random. """
 	read_data = wavio.readwav(wavefile)
 	read_array = read_data[2] # a list of sample values in sequential order for the wavefile
-	for i in range(len(onset_array)):
-		#the attack is longer than the start of the wavefile to the i'th onset 
-		#AND the decay is longer than the dist from i'th onset and end of wavefile.
-		if onset_array[i] - attack < 0 and onset_array[i] + decay > len(onset_array): 
+	for i in range(len(onset_array) - 1):
+		if onset_array[i + 1] >= len(read_array):
+			write_array = read_array[onset_array[i]:]
+			wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
+		else:
 			write_array = read_array[onset_array[i]: onset_array[i+1]]
 			wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
-		#the attack is longer than the start of the wavefile to the i'th onset
-		elif onset_array[i] - attack < 0: #the 
-			write_array = read_array[onset_array[i]: read_array[onset_array[i] + decay]]
-			wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
-		#the decay is longer than the dist from the i'th onset and the end of the wavefile.
-		elif onset_array[i] + decay > len(onset_array):
-			write_array = read_array[onset_array[i] - attack: onset_array[len(onset_array) - 1]]
-			wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
-		#the attack plus the decay do not cause indexing errors so the total drumhit{i}.wav file
-		#is of length attack + decay samples.
-		else:
-			write_array = read_array[onset_array[i] - attack: onset_array[i] + decay] 
-			wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
+
+		#the attack is longer than the start of the wavefile to the i'th onset 
+		#AND the decay is longer than the dist from i'th onset and end of wavefile.
+
+		# if onset_array[i] - attack < 0 and onset_array[i] + decay >= len(read_array): 
+		# 	write_array = read_array[onset_array[i]: onset_array[i+1]]
+		# 	wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
+		# #the attack is longer than the start of the wavefile to the i'th onset
+		# elif onset_array[i] - attack < 0: 
+		# 	write_array = read_array[onset_array[i]: onset_array[i] + decay]
+		# 	wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
+		# #the decay is longer than the dist from the i'th onset and the end of the wavefile.
+		# elif onset_array[i] + decay > len(read_array):
+		# 	write_array = read_array[onset_array[i] - attack: onset_array[len(onset_array) - 1]]
+		# 	wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
+		# #the attack plus the decay do not cause indexing errors so the total drumhit{i}.wav file
+		# #is of length attack + decay samples.
+		# else:
+		# 	write_array = read_array[onset_array[i] - attack: onset_array[i] + decay] 
+		# 	wavio.writewav24('drumhit{0}.wav'.format(i), read_data[0], write_array)
 
 extract_drums()
-
 
 # def write_clip(start, end, clipname, wavefile = sys.argv[1]):
 # 	read_data = wavio.readwav(wavefile) 

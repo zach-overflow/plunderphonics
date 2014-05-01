76b613afd920c532eb97c652de93c00135c3b74a
Zach Gottesman <zigottesman@gmail.com>
2014-04-30 19:38:39 -0700

    made drum_separator.py iterate through all files in a drum solo file and dump the drum clips into one unclassified drum file.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ plunderphonics
 ==============
 The end goal of plunderphonics is to create a new song from samples of other songs using machine learning algorithms.
 
- At the moment we are working on separating individual drum hits from an audio clip of any drum solo. These clips will be compared against a large collection of stock drum samples in order to determine what sample sounds most similar to each individual drum hit.
+ At the moment we are working on classifying individual drum hits from an audio clip of any drum solo. These clips will be compared against a large collection of stock drum samples in order to determine what sample sounds most similar to each individual drum hit.
  
  We are also developing tools for chord recognition using chroma analysis and note-to-chord matching.
 
--- wavstuff/drum_separator.py
+++ wavstuff/drum_separator.py
@@ -1,37 +1,23 @@
-import os, sys, wavio, random
+import os, sys, wavio, random, shutil
 import aubio as a
 
-"""The start and end values for these functions are in samples, not in seconds.
-   At the moment only 24 bit wav files can be used. If using AIFF files or other
-   formats, you will have to convert to 24 bit wav first."""
+"""
+The start and end values for these functions are in samples, not in seconds.
+At the moment only 24 bit wav files can be used. If using AIFF files or other
+formats, you will have to convert to 24 bit wav first.
+"""
 
-
-"""adapted from the aubio onset demo. Returns a list of onsets
-   (in samples) that appear in the wavefile."""
 win_s = 512         #fft size
 hop_s = win_s / 2   #hop size
 samplerate = 0
-filename = sys.argv[1]
-s = a.source(filename, samplerate, hop_s)
-samplerate = s.samplerate
-o = a.onset("default", win_s, hop_s, samplerate)
-onsets = [] # a list of onsets in samples
-total_frames = 0 # total number of frames read
-while True:
-	samples, read = s()
-	if o(samples):
-		#print "%f" % o.get_last_s() # for testing purposes
-		onsets.append(o.get_last())
-	total_frames += read
-	if read < hop_s: break
 
-def extract_drums(attack = 1500, onset_array = onsets, wavefile = sys.argv[1]):
+def extract_drums(attack, onset_array, wavefile):
     """
     extracts the individual drum hits from a wav file and writes them as separate wav files.
     attack and decay are in samples instead of milliseconds. So 44 samples is about 1 millisecond 
     for a second recorded at 44.1Khz samplerate.    
     The conditionals here need to be refined since the length of the subsequent wav files appear to be 
-    slightly random.
+    slightly random. It was found that an attack time of 1500 samples works well for drum transients.
     """
     read_data = wavio.readwav(wavefile)
     filename = os.path.splitext(wavefile)[0]
@@ -40,16 +26,38 @@ def extract_drums(attack = 1500, onset_array = onsets, wavefile = sys.argv[1]):
 	    if onset_array[i + 1] >= len(read_array) and onset_array[i] - attack < 0:
 	    	write_array = read_array[0:]
 	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    elif onset_array[i] - attack < 0:
 	    	write_array = read_array[0: onset_array[i + 1]]
 	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    elif onset_array[i + 1] >= len(read_array):
 	    	write_array = read_array[onset_array[i] - attack:]
 	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    else:
 	    	write_array = read_array[onset_array[i] - attack: onset_array[i+1]]
-	    	if i == 20: # for testing only
-	    		print len(write_array)
 	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 
-extract_drums()
+fileList = os.listdir('./unseparated_drums') #list of drum files in unseparated_drums directory
+
+while len(fileList) > 0: #iterate through all unseparated drum clips in unseparated_drums directory
+	filename = './unseparated_drums/{0}'.format(fileList[0])
+	s = a.source(filename, samplerate, hop_s)
+	samplerate = s.samplerate
+	o = a.onset("default", win_s, hop_s, samplerate)
+	onsets = [] # a list of onsets in samples
+	total_frames = 0 # total number of frames read
+	while True:
+		samples, read = s()
+		if o(samples):
+			#print "%f" % o.get_last_s() # for testing purposes
+			onsets.append(o.get_last())
+		total_frames += read
+		if read < hop_s: break
+	extract_drums(1500, onsets, filename)
+	del fileList[0] #remove clip from list of drum files to separate
+print 'DONE, BRO!'
+
+

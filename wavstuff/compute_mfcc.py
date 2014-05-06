d4eb2d7537735187a0feb710aff83aef3e0db1a9
Praagya Singh <praagya@berkeley.edu>
2014-05-06 15:28:57 -0700

    csv shit

---
+++ wavstuff/compute_mfcc.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+import sys, wavio, csv, os
+from aubio import source, pvoc, mfcc
+from numpy import array, vstack, zeros
+
+# fft size
+win_s = 512
+# hop size
+hop_s = win_s / 4
+n_filters = 40
+# number of MFCC coefficients. needs to be 1 more than actual number since
+# we are discarding the first coefficient
+n_coeffs = 19
+samplerate = 44100
+# milliseconds of data to compute MFCC for. 
+# we need this to ensure that our MFCC matrix has the same dimensions
+# for each drum hit file. 
+ms_for_mfcc = 50
+
+#samples_for_mfcc = int(samplerate * ms_for_mfcc * 0.001)
+
+# temporary
+samples_for_mfcc = 3100
+
+if len(sys.argv) < 2:
+    print "Usage: %s <source_directory>" % sys.argv[0]
+    sys.exit(1)
+
+source_directory = sys.argv[1]
+
+files_in_src_dir = os.listdir(source_directory)
+wav_files = filter(lambda f : '.wav' in f, files_in_src_dir)
+csv_files = filter(lambda f : '.csv' in f, files_in_src_dir)
+
+samples_to_onset_dict = {}
+
+for csvfile in csv_files:
+    with open('{0}/{1}'.format(source_directory, csvfile), 'r') as c:
+        reader = csv.reader(c)
+        for row in reader:
+            samples_to_onset_dict[os.path.basename(row[0])] = row[1]
+
+print samples_to_onset_dict
+
+'''
+# write a temporary file of length samples_for_mfcc which we compute mfcc
+readwav = wavio.readwav(source_filename)
+readwav_array = readwav[2]
+# need to refind onset here
+mfccwav_start = #CSV bullshit
+
+mfccwav_array = readwav_array[mfccwav_start: mfccwav_start + samples_for_mfcc]
+
+samplerate = 0
+if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
+
+s = source(source_filename, samplerate, hop_s)
+samplerate = s.samplerate
+p = pvoc(win_s, hop_s)
+m = mfcc(win_s, n_filters, n_coeffs, samplerate)
+
+mfccs = zeros([n_coeffs,])
+frames_read = 0
+while True:
+    samples, read = s()
+    spec = p(samples)
+    mfcc_out = m(spec)
+    mfccs = vstack((mfccs, mfcc_out))
+    frames_read += read
+    if read < hop_s: break'''

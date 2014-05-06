2a7b199aeea3058a80bbd974d89b59b23820b455
Zach Gottesman <zigottesman@gmail.com>
2014-05-05 23:36:58 -0700

    drum_separator.py now only writes files of length 3700 samples or longer.

--- wavstuff/drum_separator.py
+++ wavstuff/drum_separator.py
@@ -18,6 +18,8 @@ def extract_drums(attack, onset_array, wavefile):
     for a second recorded at 44.1Khz samplerate.    
     The conditionals here need to be refined since the length of the subsequent wav files appear to be 
     slightly random. It was found that an attack time of 1500 samples works well for drum transients.
+    If the transient is too short (2200 samples =~50 ms not including the attack time), the file for 
+    that drum hit is not written as it will not be useful.
     """
     read_data = wavio.readwav(wavefile)
     filename = os.path.splitext(wavefile)[0]
@@ -25,22 +27,26 @@ def extract_drums(attack, onset_array, wavefile):
     for i in range(len(onset_array) - 1):
 	    if onset_array[i + 1] >= len(read_array) and onset_array[i] - attack < 0:
 	    	write_array = read_array[0:]
-	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
-	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
+	    	# wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	# shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    elif onset_array[i] - attack < 0:
 	    	write_array = read_array[0: onset_array[i + 1]]
-	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
-	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
+	    	# wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	# shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    elif onset_array[i + 1] >= len(read_array):
 	    	write_array = read_array[onset_array[i] - attack:]
-	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
-	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
+	    	# wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	# shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 	    else:
 	    	write_array = read_array[onset_array[i] - attack: onset_array[i+1]]
+	    	# wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
+	    	# shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
+	    if len(write_array) - attack >= 2200: #if the drumhit file is long enough, write it into the unclassified_drums directory
 	    	wavio.writewav24('{0}_{1}.wav'.format(filename, "%05d" % i), read_data[0], write_array)
 	    	shutil.move('{0}_{1}.wav'.format(filename, "%05d" % i), 'unclassified_drums')
 
 fileList = os.listdir('./unseparated_drums') #list of drum files in unseparated_drums directory
+fileList = filter(lambda f : '.wav' in f, fileList)
 
 while len(fileList) > 0: #iterate through all unseparated drum clips in unseparated_drums directory
 	filename = './unseparated_drums/{0}'.format(fileList[0])

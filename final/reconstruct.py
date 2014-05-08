8fe7a84fb35101c4270654e098b0b6aeca9e2ced
Praagya Singh <praagya@berkeley.edu>
2014-05-08 00:15:21 -0700

    individual drum hits from input file

--- final/reconstruct.py
+++ final/reconstruct.py
@@ -32,8 +32,47 @@ while True:
 	total_frames += read
 	if read < hop_s: break
 
-# get samples to onset csv for the input file
+def extract_drums(attack, decay, onset_array, wavefile):
+    """ 
+    extracts the individual drum hits from a wav file and writes them as separate wav files.
+    attack and decay are in samples instead of milliseconds. So 44 samples is about 1 millisecond 
+    for a second recorded at 44.1Khz samplerate.    
+    The conditionals here need to be refined since the length of the subsequent wav files appear to be 
+    slightly random. It was found that an attack time of 1500 samples works well for drum transients.
+    If the transient is too short (2200 samples =~50 ms not including the attack time), the file for 
+    that drum hit is not written as it will not be useful.
+    """
+    read_data = wavio.readwav(wavefile)
+    filename = os.path.splitext(wavefile)[0]
+    read_array = read_data[2] # a list of sample values in sequential order for the wavefile
+    output_csv_filename = '{0}_samples_to_onset.csv'.format(filename)
+    samples_to_onset_array = []
+    output_dir = 'separated_input'
+    for i in range(len(onset_array) - 1): 
+        if onset_array[i + 1] >= len(read_array) and onset_array[i] - attack < 0:
+            start = 0 
+            write_array = read_array[0:]
+        elif onset_array[i] - attack < 0:
+            start = 0 
+            write_array = read_array[0: onset_array[i + 1]] 
+        elif onset_array[i + 1] >= len(read_array):
+            start = onset_array[i] - attack
+            write_array = read_array[onset_array[i] - attack:]
+        else:
+            start = onset_array[i] - attack
+            write_array = read_array[onset_array[i] - attack: onset_array[i+1]]
+        if len(write_array) - attack >= decay: #if the drumhit file is long enough, write it into the unclassified_drums directory
+            output_filename = '{0}_{1}.wav'.format(filename, "%05d" % i)
+            wavio.writewav24(output_filename, read_data[0], write_array)
+            shutil.move(output_filename, '{0}'.format(output_dir))
+            samples_to_onset = onset_array[i] - start
+            samples_to_onset_array.append([output_filename, samples_to_onset])
+    with open(output_csv_filename, 'w') as csvfile:
+        a = csv.writer(csvfile)
+        a.writerows(samples_to_onset_array)
+    shutil.move(output_csv_filename, '{0}'.format(output_dir))
 
+extract_drums(attack, 3100, onsets, filename)
 
 def main():
 	"""
